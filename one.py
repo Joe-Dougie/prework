8d52e0928d1d4d4bb4e73febe79561f7c67c1c18
Douglas Camacho <douglascamacho1110@yahoo.com>
2022-03-28 15:16:36 -0500

    first commit

---
+++ one.py
@@ -0,0 +1,36 @@
+# Question 1: 
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below. 
+def hello_name(user_name):
+    user_name == "douglas"
+print(user_name)
+
+# Question 2:
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing def first_odds():
+numbers = list(range(1,101))
+print(numbers)
+odd_numbers = list(range(1,100,2))
+print(odd_numbers)
+
+# Question 3:
+# Please write a python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+def max_num_in_list(a_list):
+    a_list = list(range (1,100))
+print(numbers)
+max(numbers)
+
+# Question 4:
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+def is_leap_year(a_year):
+    current_year = 1992
+    while current_year <= 2040:
+        print(current_year)
+        current_year += 4
+
+# Question 5:
+# Write a function to check to see if all numbers in the list are coseccutive numbers. For example,[2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The returnshould be boolean Type.
+
+def is_consecutive(a_list):
+    current_number = 1
+    while current_number <= 8:
+        print(current_number)
+        current_number += 1
